b46315dbf0217f390bb4913ffc318403c9bb67c3
Ricardo D. Quiroga <l2radamanthys@gmail.com>
2024-05-19 19:58:23 -0300

    scalar

--- src/graphql/index.js
+++ src/graphql/index.js
@@ -5,13 +5,23 @@ const typeDefs = `
   type Query {
     hello: String
     getPerson(name: String, age: Int): String
+    getInt(age: Int): Int
+    getFloat(price: Float): Float
+    getString: String
+    getBoolean: Boolean
+    getID: ID
   }
 `;
 
 const resolvers = {
   Query: {
     hello: () => 'Hola Mundo',
-    getPerson: (_, args) => `Hello my name is ${args.name}, I'm ${args.age} years old.`
+    getPerson: (_, args) => `Hello my name is ${args.name}, I'm ${args.age} years old.`,
+    getInt: (_, args) => args.age,
+    getFloat: (_, args) => args.price,
+    getString: () => 'Palabra',
+    getBoolean: () => true,
+    getID: () => '121313424234'
   }
 }
 
