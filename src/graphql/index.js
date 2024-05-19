0c1a70ddeb8d175e5111a7cbd622d0f57b6bbd68
Ricardo D. Quiroga <l2radamanthys@gmail.com>
2024-05-19 20:14:03 -0300

    listas y manejo de valores nulos

--- src/graphql/index.js
+++ src/graphql/index.js
@@ -3,25 +3,34 @@ const { ApolloServerPluginLandingPageGraphQLPlayground } = require('apollo-serve
 
 const typeDefs = `
   type Query {
-    hello: String
+    hello: String!
     getPerson(name: String, age: Int): String
-    getInt(age: Int): Int
+    getInt(age: Int!): Int
     getFloat(price: Float): Float
     getString: String
     getBoolean: Boolean
     getID: ID
+    getNumbers(numbers: [Int!]!): [Int]
   }
 `;
 
+// GET: Query
+// POST, PUT, DELETE: Mutations
+
+// Lists
+// [String]
+// [Int]
+
 const resolvers = {
   Query: {
-    hello: () => 'Hola Mundo',
+    hello: () => 'Hola mundo',
     getPerson: (_, args) => `Hello my name is ${args.name}, I'm ${args.age} years old.`,
     getInt: (_, args) => args.age,
     getFloat: (_, args) => args.price,
     getString: () => 'Palabra',
     getBoolean: () => true,
-    getID: () => '121313424234'
+    getID: () => '121313424234',
+    getNumbers: (_, args) => args.numbers
   }
 }
 
