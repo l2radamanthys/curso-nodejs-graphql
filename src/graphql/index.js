ac903f8fa5b7a2d01a40c28f937ffbad8abd741e
Ricardo D. Quiroga <l2radamanthys@gmail.com>
2024-05-19 19:21:01 -0300

    inicializando servidor graphql

---
+++ src/graphql/index.js
@@ -0,0 +1,31 @@
+const { ApolloServer } = require('apollo-server-express');
+const { ApolloServerPluginLandingPageGraphQLPlayground } = require('apollo-server-core');
+const { Query } = require('pg');
+
+const typeDefs = `
+  type Query {
+    hello: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => 'Hola Mundo'
+  }
+}
+
+const useGraphql = async (app) => {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    playground: true,
+    plugins: [
+      ApolloServerPluginLandingPageGraphQLPlayground
+    ]
+  });
+  await server.start();
+  server.applyMiddleware({ app })
+};
+
+
+module.exports = useGraphql;
--- src/routes/index.js
+++ src/routes/index.js
@@ -8,6 +8,7 @@ const customersRouter = require('./customers.router');
 const authRouter = require('./auth.router');
 const profileRouter = require('./profile.router');
 
+
 function routerApi(app) {
   const router = express.Router();
   app.use('/api/v1', router);
