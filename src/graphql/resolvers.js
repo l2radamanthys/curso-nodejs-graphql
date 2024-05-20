5904fcb051e9a52cb3d309ac15f127ebd4349bfc
Ricardo D. Quiroga <l2radamanthys@gmail.com>
2024-05-20 05:37:56 -0300

    scalars

--- src/graphql/resolvers.js
+++ src/graphql/resolvers.js
@@ -1,6 +1,11 @@
 const { getProduct, getProducts, addProduct, deleteProduct, updateProduct } = require('./product.resolvers');
 const { login } = require('./auth.resolvers');
 const { addCategory } = require('./category.resolvers');
+const { RegularExpression } = require('graphql-scalars');
+
+
+const CategoryNameType = new RegularExpression('CategoryNameType', /^[a-zA-Z0-9]{3,8}$/)
+
 
 const resolvers = {
   Query: {
@@ -25,7 +30,8 @@ const resolvers = {
     deleteProduct,
     // Category
     addCategory,
-  }
+  },
+  CategoryNameType,
 }
 
 module.exports = resolvers;
